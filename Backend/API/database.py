30e41a63c4b8dc6b54ee0a3737ccd0b2f15a9134
Noé <noeebacke@gmail.com>
2023-04-03 15:42:42 +0200

    oush database

---
+++ Backend/API/database.py
@@ -0,0 +1,11 @@
+
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASES_URL:str = 'mysql+pymysql://root:123456@localhost:3306/db_api_rest'
+
+engine = create_engine(SQLALCHEMY_DATABASES_URL, echo=True)
+
+LocalSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
