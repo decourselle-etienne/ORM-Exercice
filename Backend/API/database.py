cd76b21f7fd6dad93d92fa2f79cbe025658522bf
Etienne Decourselle <decourselle.etienne@hotmail.fr>
2023-04-03 16:42:19 +0200

    database MAJ

--- Backend/API/database.py
+++ Backend/API/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASES_URL: str = 'mysql+pymysql://root:123456@localhost:3306/db_ORM_exercice'
+SQLALCHEMY_DATABASES_URL: str = 'mysql+pymysql://root:123456@localhost:3306/company'
 
 engine = create_engine(SQLALCHEMY_DATABASES_URL, echo=True)
 
